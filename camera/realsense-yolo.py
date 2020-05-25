8525c2ffbdace6cd6fc3d1416bc0e4a4e1a33b2c
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2020-05-25 09:41:43 +0200

    Added YOLO example with Realsense camera

--- camera/realsense-yolo.py
+++ camera/realsense-yolo.py
@@ -3,3 +3,134 @@ import time
 import cv2
 import numpy as np
 from model.yolo_model import YOLO
+
+def process_image(img):
+    image = cv2.resize(img, (416, 416),
+                       interpolation=cv2.INTER_CUBIC)
+    image = np.array(image, dtype='float32')
+    image /= 255.
+    image = np.expand_dims(image, axis=0)
+
+    return image
+
+def get_classes(file):
+    with open(file) as f:
+        class_names = f.readlines()
+    class_names = [c.strip() for c in class_names]
+
+    return class_names
+
+def draw(image, boxes, scores, classes, all_classes):
+    for box, score, cl in zip(boxes, scores, classes):
+        x, y, w, h = box
+
+        top = max(0, np.floor(x + 0.5).astype(int))
+        left = max(0, np.floor(y + 0.5).astype(int))
+        right = min(image.shape[1], np.floor(x + w + 0.5).astype(int))
+        bottom = min(image.shape[0], np.floor(y + h + 0.5).astype(int))
+
+        cv2.rectangle(image, (top, left), (right, bottom), (255, 0, 0), 2)
+        cv2.putText(image, '{0} {1:.2f}'.format(all_classes[cl], score),
+                    (top, left - 6),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.6, (0, 0, 255), 1,
+                    cv2.LINE_AA)
+
+        print('class: {0}, score: {1:.2f}'.format(all_classes[cl], score))
+        print('box coordinate x,y,w,h: {0}'.format(box))
+
+    print()
+
+
+def detect_image(image, yolo, all_classes):
+    pimage = process_image(image)
+
+    start = time.time()
+    boxes, classes, scores = yolo.predict(pimage, image.shape)
+    end = time.time()
+
+    print('time: {0:.2f}s'.format(end - start))
+
+    if boxes is not None:
+        draw(image, boxes, scores, classes, all_classes)
+
+    return image
+
+
+def detect_video(video, yolo, all_classes):
+    video_path = os.path.join("videos", "test", video)
+    camera = cv2.VideoCapture(video_path)
+    cv2.namedWindow("detection", cv2.WINDOW_AUTOSIZE)
+
+    # Prepare for saving the detected video
+    sz = (int(camera.get(cv2.CAP_PROP_FRAME_WIDTH)),
+          int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    fourcc = cv2.VideoWriter_fourcc(*'mpeg')
+
+    vout = cv2.VideoWriter()
+    vout.open(os.path.join("videos", "res", video), fourcc, 20, sz, True)
+
+    while True:
+        res, frame = camera.read()
+
+        if not res:
+            break
+
+        image = detect_image(frame, yolo, all_classes)
+        cv2.imshow("detection", image)
+
+        # Save the video frame by frame
+        vout.write(image)
+
+        if cv2.waitKey(110) & 0xff == 27:
+            break
+
+    vout.release()
+    camera.release()
+
+yolo = YOLO(0.5, 0.5)
+file = 'data/coco_classes.txt'
+all_classes = get_classes(file)
+
+# f = 'people-airport.jpg'
+# path = 'images/'+f
+# image = cv2.imread(path)
+# image = detect_image(image, yolo, all_classes)
+# cv2.imwrite('images/res/' + f, image)
+
+# video = 'schiphol.mp4'
+# detect_video(video, yolo, all_classes)
+
+import pyrealsense2 as rs
+
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+# Start streaming
+pipeline.start(config)
+
+try:
+    while True:
+
+        # Wait for a coherent pair of frames: depth and color
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+        if not depth_frame or not color_frame:
+            continue
+
+        color_image = np.asanyarray(color_frame.get_data())
+
+        color_image = detect_image(color_image, yolo, all_classes)
+
+        cv2.namedWindow('RealSense-YOLO', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('RealSense-YOLO', color_image)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+finally:
+    # Stop streaming
+    pipeline.stop()
