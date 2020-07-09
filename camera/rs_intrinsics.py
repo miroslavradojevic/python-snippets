71c632fc393d5389093c2fdae3b79965554bcb73
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2020-07-09 14:19:40 +0200

    Grab intrinsic parameters of RealSenseD435i

--- camera/rs_intrinsics.py
+++ camera/rs_intrinsics.py
@@ -1,8 +1,8 @@
 # https://github.com/IntelRealSense/librealsense/blob/master/wrappers/python/examples/align-depth2color.py
+# https://github.com/IntelRealSense/realsense-ros/issues/709
+# http://docs.ros.org/melodic/api/sensor_msgs/html/msg/CameraInfo.html
 
 import pyrealsense2 as rs
-# import numpy as np
-# import cv2
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
@@ -16,8 +16,13 @@ try:
     profile = cfg.get_stream(rs.stream.depth) # Fetch stream profile for depth stream
     intr = profile.as_video_stream_profile().get_intrinsics() # Downcast to video_stream_profile and fetch intrinsics
 
-    print(profile.as_video_stream_profile())
-    print("cx = ", intr.ppx, " cy = ", intr.ppy)
+    # print(profile.as_video_stream_profile())
+    # print(type(intr))
+    print("\n", intr, "\n")
+    print("principal point: cx = ", intr.ppx, " cy = ", intr.ppy)
+    print("focal lengths: fx = ", intr.fx, " fy = ", intr.fy)
+    print("model = ", intr.model)
+    print("distortion coefficients", intr.coeffs)
 
 finally:
     pipeline.stop()
