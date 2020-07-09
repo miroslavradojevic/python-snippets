301b4eed92c20926e794b9eb0b3d14fc4028c6b7
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2020-07-09 15:22:25 +0200

    Added hints on installing pygame

--- jetbot/teleoperation_joystic.py
+++ jetbot/teleoperation_joystic.py
@@ -3,6 +3,24 @@ import sys
 
 from jetbot import Robot
 
+# install pygame dependencies
+# https://stackoverflow.com/questions/7652385/where-can-i-find-and-install-the-dependencies-for-pygame
+# sudo apt-get install python-dev \
+# libsdl-image1.2-dev \
+# libsdl-mixer1.2-dev \
+# libsdl-ttf2.0-dev \
+# libsdl1.2-dev \
+# libsmpeg-dev \
+# python-numpy \
+# subversion \
+# libportmidi-dev \
+# ffmpeg \
+# libswscale-dev \
+# libavformat-dev \
+# libavcodec-dev
+
+# libfreetype6-dev
+
 pygame.init()
 pygame.joystick.init()
 
