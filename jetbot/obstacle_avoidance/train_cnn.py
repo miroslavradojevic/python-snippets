877086c98ccbc9a0008e07f43d669422d776b2fd
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2020-07-19 10:11:21 +0200

    Initialized script for training cnn network for obstacle avoidance

--- jetbot/obstacle_avoidance/train_cnn.py
+++ jetbot/obstacle_avoidance/train_cnn.py
@@ -1,1 +1,12 @@
 
+import sys
+import os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+data_dir = "/home/miro/stack/nuctech/jetbot_obstacle_avoidance"
+
+
+if __name__ == "__main__":
+    print(os.listdir(data_dir))
+    print(len(sys.argv), " ", sys.argv[0])
+    print(help(ImageDataGenerator))
