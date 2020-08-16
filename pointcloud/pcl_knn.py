5783999f3d03b137fd52dff807b6858b32499b02
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2020-08-16 10:14:00 +0200

    Removed comment

--- pointcloud/pcl_knn.py
+++ pointcloud/pcl_knn.py
@@ -2,8 +2,7 @@
 import open3d as o3d
 import numpy as np
 
-# Load point cloud
-# http://www.open3d.org/docs/release/tutorial/Basic/file_io.html
+
 # pcd = o3d.io.read_point_cloud("/home/miro/stack/nuctech/progress/200810/l-cas/1464002845.824573000.pcd", format='pcd')
 pcd = o3d.io.read_point_cloud("/home/miro/stack/nuctech/progress/200810/kucl_29/_scan0029.txt", format='xyz')
 
@@ -15,8 +14,6 @@ pcd_array = np.asarray(pcd.points)
 
 # KDTree
 pcd_tree = o3d.geometry.KDTreeFlann(pcd)
-
-nr_points = pcd_array.shape[0]
 
 # Paint the random point red
 point_idx = np.random.randint(nr_points)
