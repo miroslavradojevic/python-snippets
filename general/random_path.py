c94361208d6e74b75f92b7972c6f9f8790eb7b3a
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2021-03-29 00:54:58 +0200

    Random path generator wrapped in a separate function

--- general/random_path.py
+++ general/random_path.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# generate random (x,y) path
+### generate random (x,y) path
 import random
 import math
 import numpy as np
@@ -10,12 +10,30 @@ thetaSigma = np.pi/5
 
 path = []
 path.append([0, 0, 0]) # x, y, theta
-# print(path, type(path), len(path))
 
 def wrap(a):
     # https://stackoverflow.com/questions/58627711/get-angle-into-range-0-2pi-python
     a = a % (2*math.pi)  # wrap in [0, 2pi) range
     return a
+
+def generate_trajectory(x0, y0, theta0, Npoints, path_step, path_style):
+    path = []
+    path.append([x0, y0, theta0])
+    
+    for i in range(Npoints):
+        if (path_style=="random_gauss"):
+            theta1 = wrap(path[-1][2] + random.gauss(0, np.pi/4))
+            x1 = path[-1][0] + path_step * math.cos(theta1)
+            y1 = path[-1][1] + path_step * math.sin(theta1)
+            path.append([x1, y1, theta1])
+    
+    out = np.asarray(path)[:,:2]
+
+    print("points shape {}".format(out.shape))
+
+    return out
+
+p = generate_trajectory(0, 0, 0, N, step, "random_gauss")
 
 for i in range(N):
     theta0 = path[-1][2]
@@ -27,13 +45,9 @@ for i in range(N):
     y1 = y0 + step * math.sin(theta1)
 
     path.append([x1, y1, theta1])
-    # print(np.asarray(path).shape)
 
 xPath = np.asarray(path)[:,0]
 yPath = np.array(path)[:,1]
-# print(xPath.shape)
-# print(np.asarray(path).shape)
-
 
 fig = plt.figure(figsize=(8, 8))
 plt.plot(xPath, yPath, marker='o', markersize=1)
