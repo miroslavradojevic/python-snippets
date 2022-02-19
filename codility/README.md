590d593dd782a0584ada13c84c812b1c4fe269b2
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2022-02-19 11:26:23 +0100

    Added readme and odd occurences computation script

---
+++ codility/README.md
@@ -0,0 +1,2 @@
+Codility developer training
+https://app.codility.com/programmers/lessons
---
+++ codility/odd_occurrences_in_array.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Find value that occurs in odd number of elements
+
+def oddOccurrencesInArray():
+    pass
+
+if __name__=='__main__':
+    oddOccurrencesInArray()
